6dcb8ed697c093ad766ff570d3141dcf72d83d8c
Madhusudhan Konda <m.konda@chocolateminds.com>
2023-06-11 16:05:01 +0100

    Hardcoded Symptoms Checker

---
+++ main.js
@@ -0,0 +1,23 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: "YOUR_KEY",
+});
+
+// create an instance of OpenAIApi class
+const openai = new OpenAIApi(configuration);
+
+// Hardcode prompt
+const symptoms =  "Top 5 Flu Symptoms as a table"
+
+// Invoke the API endpoint
+openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: symptoms }],
+  })
+  .then((res) => {
+    console.log(res.data.choices[0].message.content);
+  })
+  .catch((e) => {
+    console.log(e);
+});
